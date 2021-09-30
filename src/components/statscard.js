8a27f05dec788357d5ff87f6efda4195327b4f76
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-09-30 12:14:27 +0100

    finished overview page

---
+++ src/components/statscard.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import clock from "../assets/clock.svg";
+import greenarrow from "../assets/greenarrow.svg";
+
+const Container = styled.div`
+  background: #f0fafa;
+  margin: 5% auto;
+  padding: 20px 0;
+  width: 90%;
+  span {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
+    div {
+      font-family: Roboto;
+      color: #838383;
+      line-height: 0.5;
+      padding: 2% 5%;
+      text-align: left;
+      background: white;
+      width: 35%;
+      border-radius: 5px;
+
+      h4 {
+        font-size: 13px;
+        font-weight: bold;
+      }
+
+      p {
+        font-size: 16px;
+        font-weight: normal;
+      }
+    }
+  }
+
+  .invoice {
+    margin: 0;
+    margin-top: 20px;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 80%;
+
+    p {
+      margin: 0;
+      font-size: inherit;
+      font-family: Poppins;
+    }
+    a {
+      text-decoration: none;
+      color: #489f9f;
+    }
+  }
+`;
+
+const StatsCard = ({ received, pending }) => {
+  return (
+    <>
+      <Container>
+        <span>
+          <div>
+            <h4>Received</h4>
+            <p>NGN {received}</p>
+          </div>
+          <div>
+            <h4>Pending</h4>
+            <p>NGN {pending}</p>
+          </div>
+        </span>
+        <div className="invoice">
+          <img src={clock} alt="clock icon" />
+          <p>
+            <Link to="/">View all Invoices</Link>
+          </p>
+          <img src={greenarrow} alt="forward icon" />
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default StatsCard;
--- src/pages/overview.js
+++ src/pages/overview.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../components/header.js";
+import StatsCard from "../components/statscard.js";
 import profilecard from "../assets/profilecard.svg";
 import forwardicon from "../assets/forwardicon.svg";
+import greaterthan from "../assets/greaterthan.svg";
 
 const Container = styled.div`
   text-align: center;
@@ -24,8 +26,41 @@ const Button = styled.button`
   border: none;
   width: 90%;
   margin: 0 auto;
-  padding: 0 5%;
+  padding: 3px 5%;
   border-radius: 5px;
+  p {
+    font-weight: bold;
+    font-family: Poppins;
+  }
+`;
+
+const Leads = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  margin-bottom: 100px;
+  color: #bbbbbb;
+  font-size: 16px;
+  font-weight: bold;
+
+  h4 {
+    text-align: left;
+  }
+  ul {
+    width: 100%;
+    padding: 0;
+    list-style-type: circle;
+    color: #838383;
+    li {
+      margin: 10px 0;
+      padding: 0 5%;
+      border: 1px solid #e3e3e3;
+      border-radius: 5px;
+      font-size: 14px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
 `;
 
 const Overview = () => {
@@ -38,6 +73,18 @@ const Overview = () => {
           <p>Request a payment</p>
           <img src={forwardicon} alt="go" />
         </Button>
+        <StatsCard received={1000} pending={2000} />
+        <Leads>
+          <h4>Leads</h4>
+          <ul>
+            <li>
+              <h4>John Doe</h4> <img src={greaterthan} alt="greater than" />
+            </li>
+            <li>
+              <h4>John Doe</h4> <img src={greaterthan} alt="greater than" />
+            </li>
+          </ul>
+        </Leads>
       </Container>
     </>
   );
