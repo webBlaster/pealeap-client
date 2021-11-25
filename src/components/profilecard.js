d2fe9490fd38cfc7cadc4f6cab8fd73048c68c60
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-11-25 14:26:55 +0100

    added edit link to profile card

--- src/components/profilecard.js
+++ src/components/profilecard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Profile = styled.div`
@@ -63,7 +64,13 @@ const ProfileCard = ({ image, name, number, edit }) => {
             <p>{number}</p>
           </div>
         </span>
-        <p>{edit ? "edit" : null}</p>
+        <p>
+          {edit ? (
+            <Link to="/settings">
+              <p>edit</p>
+            </Link>
+          ) : null}
+        </p>
       </Profile>
     </>
   );
